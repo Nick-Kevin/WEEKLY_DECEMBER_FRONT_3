93fc3605f775196009b1869364ec885a01a6af9b
Nick-Kevin <nicblou23@gmail.com>
2023-12-16 07:57:57 +0000

    update navbar

--- src/components/StickyNavbar.jsx
+++ src/components/StickyNavbar.jsx
@@ -64,7 +64,7 @@ export default function StickyNavbar() {
   );
  
   return (
-      <Navbar className="sticky top-0 z-10 max-w-full rounded-none px-4 py-2 lg:px-8 lg:py-4">
+      <Navbar className="sticky top-0 z-10 max-w-full rounded-none px-4 py-2 md:px-16 lg:px-24 lg:py-2">
         <div className="flex items-center justify-between text-blue-gray-900">
           <Typography
             as="a"
