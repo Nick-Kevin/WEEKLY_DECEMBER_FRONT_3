f7cd52cd3c2e00ddc2ffecd12629518c904c69de
Nick-Kevin <nicblou23@gmail.com>
2023-12-16 12:10:28 +0000

    update first section

--- src/App.jsx
+++ src/App.jsx
@@ -3,33 +3,50 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import StickyNavbar from './components/StickyNavbar'
+import FlexColThreeRows from './components/FlexColThreeRows'
+import Button from './components/Button'
 
 function App() {
   const [count, setCount] = useState(0)
 
+  const firstRowInHeader = () => {
+    return (
+      <>
+        <h1 className="f-bold text-5xl tracking-tight md:text-6xl lg:text-8xl">
+          Your Daily <br />
+          <span className="red-text">Podcast</span>
+        </h1>
+      </>
+    )
+  }
+
+  const secondRowInHeader = () => {
+    return (
+      <p className="w-80 px-1">
+        We cover all kinds of categories and
+        a weekly special guest.
+      </p>
+    )
+  }
+
+  const thirdRowInHeader = () => {
+    return (
+      <Button 
+        className="f-bold text-sm border-black border-2 mt-2 px-10 py-5 guide-shadow-2 border-solid text-white bg-black lg:inline-block hover:bg-transparent hover:text-black hover:border-black"
+        label="SUBSCRIBE"
+      />
+    )
+  }
+
   return (
     <>
       <StickyNavbar/>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <FlexColThreeRows
+        otherClass = "mt-8 lg:mt-11 gap-y-10"
+        firstRow = {firstRowInHeader()}
+        secondRow = {secondRowInHeader()}
+        thirdRow = {thirdRowInHeader()}
+      />
     </>
   )
 }
