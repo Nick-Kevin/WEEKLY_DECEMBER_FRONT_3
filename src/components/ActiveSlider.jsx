eadd080a7a63004e7a79bab26f73de199dbbe837
Nick-Kevin <nicblou23@gmail.com>
2023-12-16 15:49:11 +0000

    last update

--- src/components/ActiveSlider.jsx
+++ src/components/ActiveSlider.jsx
@@ -5,9 +5,17 @@ import "swiper/css/pagination"
 import "swiper/css/free-mode"
 
 import { FreeMode, Pagination } from "swiper/modules"
+
 import CoverCard from "./CoverCard"
+
 import Tesla from '../assets/header/covers/tesla-autopilot/cover.png'
 import TeslaVector from '../assets/header/covers/tesla-autopilot/vector.png'
+import Self from '../assets/header/covers/self-confidence/cover.png'
+import SelfVector from '../assets/header/covers/self-confidence/vector.png'
+import PMind from '../assets/header/covers/preplexed-mind/cover.png'
+import PMindVector from '../assets/header/covers/preplexed-mind/vector.png'
+import Womens from '../assets/header/covers/womens-rights/cover.png'
+import WomensVector from '../assets/header/covers/womens-rights/vector.png'
 
 function ActiveSlider () {
     return (
@@ -31,44 +39,33 @@ function ActiveSlider () {
                     <CoverCard
                         bgUrl={Tesla}
                         vector={TeslaVector}
+                        vectorWidth="w-40 lg:w-auto"
                         title="Tesla Autopilot"
                     />
                 </SwiperSlide>
                 <SwiperSlide>
-                    <div className="flex flex-col gap-6 mb-20 group relative shadow-lg text-white rounded-xl px-6 py-8 h-[200px] w-[200px] lg:h-[360px] lg:w-[360px] overflow-hidden">
-                        <div
-                            className="absolute inset-0 bg-cover bg-center"                            
-                            style={{ background: 'blue' }}
-                        />
-                        <div className="absolute inset-0 bg-black opacity-10 group-hover:opacity-50" />
-                        <div className="relative flex flex-col gap-3">
-                            <h1 className="text-xl lg:text-2xl">blue </h1>
-                        </div>
-                    </div>
+                    <CoverCard
+                        bgUrl={Self}
+                        vector={SelfVector}
+                        vectorWidth="w-28 z-0 lg:w-auto absolute left-14 top-4 top-0 lg:left-28 lg:top-10"
+                        title="Self-confidence"
+                    />
                 </SwiperSlide>
                 <SwiperSlide>
-                    <div className="flex flex-col gap-6 mb-20 group relative shadow-lg text-white rounded-xl px-6 py-8 h-[200px] w-[200px] lg:h-[360px] lg:w-[360px] overflow-hidden cursor-pointer">
-                        <div
-                            className="absolute inset-0 bg-cover bg-center"
-                            style={{ background: 'red' }}
-                        />
-                        <div className="absolute inset-0 bg-black opacity-10 group-hover:opacity-50" />
-                        <div className="relative flex flex-col gap-3">
-                            <h1 className="text-xl lg:text-2xl">red </h1>
-                        </div>
-                    </div>
+                    <CoverCard
+                        bgUrl={PMind}
+                        vector={PMindVector}
+                        vectorWidth="w-20 lg:w-36 ml-12 mt-2 lg:ml-[80px] lg:mt-4"
+                        title="Perplexed Mind"
+                    />
                 </SwiperSlide>
                 <SwiperSlide>
-                    <div className="flex flex-col gap-6 mb-20 group relative shadow-lg text-white rounded-xl px-6 py-8 h-[250px] w-[215px] lg:h-[400px] lg:w-[350px] overflow-hidden cursor-pointer">
-                        <div
-                            className="absolute inset-0 bg-cover bg-center"
-                            style={{ background: 'yellow' }}
-                        />
-                        <div className="absolute inset-0 bg-black opacity-10 group-hover:opacity-50" />
-                        <div className="relative flex flex-col gap-3">
-                            <h1 className="text-xl lg:text-2xl">yellow </h1>
-                        </div>
-                    </div>
+                    <CoverCard
+                        bgUrl={Womens}
+                        vector={WomensVector}
+                        vectorWidth="w-48 z-0 lg:w-auto absolute left-0 top-0"
+                        title="Womens Rights"
+                    />
                 </SwiperSlide>
                 <SwiperSlide>
                     <div className="flex flex-col gap-6 mb-20 group relative shadow-lg text-white rounded-xl px-6 py-8 h-[250px] w-[215px] lg:h-[400px] lg:w-[350px] overflow-hidden cursor-pointer">
--- src/components/CoverCard.jsx
+++ src/components/CoverCard.jsx
@@ -8,13 +8,13 @@ function CoverCard (props) {
             className="relative rounded-xl flex bg-cover flex-col justify-between p-3 lg:p-6 h-[200px] w-[200px] lg:h-[360px] lg:w-[360px] overflow-hidden"
             style={{backgroundImage: `url("${props.bgUrl}")`}}
         >
-            <div className="flex w-full gap-x-3 justify-end">
+            <div className="flex z-20 w-full gap-x-3 justify-end">
                 <img src={Google} className=" w-4 lg:w-auto cursor-pointer" alt="google"/>                
                 <img src={Spotify} className="w-4 lg:w-auto cursor-pointer" alt="spotify"/>
                 <img src={Youtube} className="w-4 lg:w-auto cursor-pointer" alt="youtube"/>
             </div>
-            <img src={props.vector} className="w-40 lg:w-auto" alt="vector" />
-            <h2 className="w-full text-start pointer pewter-blue-text text-xl lg:text-4xl f-bold tracking-tighter">{props.title}</h2>
+            <img src={props.vector} className={`${props.vectorWidth}`} alt="vector" />
+            <h2 className="w-full z-20 text-start pointer pewter-blue-text text-xl lg:text-4xl f-bold tracking-tighter">{props.title}</h2>
         </div>
     )
 }
